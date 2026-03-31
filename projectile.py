0d712a555a1d521efa0d6c7a94280b983b4fdfa8
Dillan <27839664@sun.ac.za>
2026-03-31 16:04:53 +0200

    Made Projectile_Manager class and cleaned up main

--- projectile.py
+++ projectile.py
@@ -2,6 +2,14 @@ import stddraw
 import math
 
 
+# 31/03/26: Dillan van Wyk: Added function to check for collisions and implemented it
+def collision(projectile, enemy):
+    dx = projectile.x - enemy.x
+    dy = projectile.y - enemy.y
+    distance = (dx**2 + dy**2) ** 0.5
+    return distance <= (enemy.radius + projectile.radius)
+
+
 # 31/03/26: Dillan van Wyk: Made the Projectile class and the move() and draw() functions
 class Projectile:
     x = 0
@@ -19,3 +27,40 @@ class Projectile:
         stddraw.setPenColor(stddraw.WHITE)
         if self.alive:
             stddraw.filledCircle(self.x, self.y, self.radius)
+
+
+# 31/03/26: Dillan van Wyk: Created the Project_Manager class to contain and manage functions related to projectiles [update(), draw(), shoot()]
+class Projectile_Manager:
+    projectiles = []
+    max_cooldown = 15
+    cooldown = 0
+
+    def update(self, x_min, x_max, y_min, y_max, enemies):
+        if self.cooldown > 0:
+            self.cooldown -= 1
+
+        # Projectile collisions with enemies
+        for p in self.projectiles[:]:
+            for e in enemies:
+                if e.alive and collision(p, e):
+                    e.alive = False
+                    self.projectiles.remove(p)
+                    break
+
+        # Projectile off-screen removal and movement
+        for p in self.projectiles[:]:
+            if (p.x < x_min or p.x > x_max) or (p.y < y_min or p.y > y_max):
+                self.projectiles.remove(p)
+            else:
+                p.move()
+
+    # Draw projectiles
+    def draw(self):
+        for p in self.projectiles:
+            p.draw()
+
+    # Allows shooter to shoot projectiles
+    def shoot(self, shooter):
+        if self.cooldown == 0:
+            self.projectiles.append(shooter.shoot())
+            self.cooldown = self.max_cooldown
--- space_invaders.py
+++ space_invaders.py
@@ -1,31 +1,25 @@
 import stdio, stddraw
 from shooter import Shooter
 from game_manager import Game_manager
+from projectile import Projectile_Manager
 
+# Game world constants
+WIDTH = 600
+HEIGHT = 600
 
-# 31/03/26: Dillan van Wyk: Added function to check for collisions and implemented it
-def collision(projectile, enemy):
-    dx = projectile.x - enemy.x
-    dy = projectile.y - enemy.y
-    distance = (dx**2 + dy**2) ** 0.5
-    return distance <= (enemy.radius + projectile.radius)
+CANVAS_SIZE = 500
+
+# Font sizes
+TITLE_FONT = 35
+SUBTITLE_FONT = 25
+BODY_FONT = 18
 
 
 def main() -> None:
-    # Screen setup
-    x_scale_min, y_scale_min = 0, 0
-    x_scale_max, y_scale_max = 600, 600
-    x_canvas, y_canvas = 500, 500
-
-    # Font sizes
-    main_title_font_size = 0.07
-    subtitle_font_size = 0.05
-    body_font_size = 0.04
-
-    # Canvas initialization
-    stddraw.setCanvasSize(x_canvas, y_canvas)
-    stddraw.setXscale(x_scale_min, x_scale_max)
-    stddraw.setYscale(y_scale_min, y_scale_max)
+    # Canvas setup
+    stddraw.setCanvasSize(CANVAS_SIZE, CANVAS_SIZE)
+    stddraw.setXscale(0, WIDTH)
+    stddraw.setYscale(0, HEIGHT)
 
     # Shooter and enemies
     shooter = Shooter()
@@ -33,9 +27,7 @@ def main() -> None:
     manager.create_enemies()
 
     # Projectiles
-    projectiles = []
-    max_cooldown = 15
-    cooldown = max_cooldown
+    projectile_manager = Projectile_Manager()
 
     # Title screen
     # 26/02/26: Dillan van Wyk: set up the title screen
@@ -43,53 +35,21 @@ def main() -> None:
         stddraw.clear(stddraw.BLACK)
         stddraw.setPenColor(stddraw.WHITE)
 
-        stddraw.setFontSize(int(x_canvas * main_title_font_size))
-        stddraw.text(
-            (x_scale_min + x_scale_max) / 2,
-            (y_scale_min + y_scale_max) * 18 / 20,
-            "SPACE INVADERS",
-        )
+        stddraw.setFontSize(TITLE_FONT)
+        stddraw.text(WIDTH / 2, HEIGHT * 18 / 20, "SPACE INVADERS")
 
-        stddraw.setFontSize(int(x_canvas * subtitle_font_size))
-        stddraw.text(
-            (x_scale_min + x_scale_max) / 2,
-            (y_scale_min + y_scale_max) * 15 / 20,
-            "Instructions:",
-        )
+        stddraw.setFontSize(SUBTITLE_FONT)
+        stddraw.text(WIDTH / 2, HEIGHT * 15 / 20, "Instructions:")
 
-        stddraw.setFontSize(int(x_canvas * body_font_size))
-        stddraw.text(
-            (x_scale_min + x_scale_max) / 2,
-            (y_scale_min + y_scale_max) * 12 / 20,
-            "[A] move left, [S] stop move, [D] move right",
-        )
-        stddraw.text(
-            (x_scale_min + x_scale_max) / 2,
-            (y_scale_min + y_scale_max) * 11 / 20,
-            "[Q] rotate left, [W] stop rotate, [E] rotate right",
-        )
-        stddraw.text(
-            (x_scale_min + x_scale_max) / 2,
-            (y_scale_min + y_scale_max) * 10 / 20,
-            "[Space] to shoot",
-        )
-        stddraw.text(
-            (x_scale_min + x_scale_max) / 2,
-            (y_scale_min + y_scale_max) * 8 / 20,
-            "[H] for help",
-        )
-        stddraw.text(
-            (x_scale_min + x_scale_max) / 2,
-            (y_scale_min + y_scale_max) * 7 / 20,
-            "[X] to quit",
-        )
+        stddraw.setFontSize(BODY_FONT)
+        stddraw.text(WIDTH / 2, HEIGHT * 12 / 20, "[A] move left, [D] move right")
+        stddraw.text(WIDTH / 2, HEIGHT * 11 / 20, "[Q] rotate left, [E] rotate right")
+        stddraw.text(WIDTH / 2, HEIGHT * 10 / 20, "[Space] to shoot")
+        stddraw.text(WIDTH / 2, HEIGHT * 8 / 20, "[H] for help")
+        stddraw.text(WIDTH / 2, HEIGHT * 7 / 20, "[X] to quit")
 
-        stddraw.setFontSize(int(x_canvas * subtitle_font_size))
-        stddraw.text(
-            (x_scale_min + x_scale_max) / 2,
-            (y_scale_min + y_scale_max) * 4 / 20,
-            "Press any key to start",
-        )
+        stddraw.setFontSize(SUBTITLE_FONT)
+        stddraw.text(WIDTH / 2, HEIGHT * 4 / 20, "Press any key to start")
 
         stddraw.show(20)
 
@@ -115,36 +75,13 @@ def main() -> None:
             elif key == "e":
                 shooter.rotate_right()
             elif key == " ":
-                if cooldown == 0:
-                    projectiles.append(shooter.shoot())
-                    cooldown = max_cooldown
+                projectile_manager.shoot(shooter)
             elif key == "x":
                 break
 
-        # Cooldown countdown
-        if cooldown > 0:
-            cooldown -= 1
-
-        # 31/03/26: Dillan van Wyk: Projectile collisions with enemies
-        for p in projectiles[:]:
-            for e in manager.enemies:
-                if e.alive and collision(p, e):
-                    e.alive = False
-                    projectiles.remove(p)
-                    break
-
-        # 31/03/26: Dillan van Wyk: Projectile movement, projectile drawing and off-screen removal
-        for i in projectiles[:]:
-            if (i.x < x_scale_min or i.x > x_scale_max) or (
-                i.y < y_scale_min or i.y > y_scale_max
-            ):
-                projectiles.remove(i)
-            else:
-                i.move()
-
-        # Draw projectiles
-        for i in projectiles:
-            i.draw()
+        # update and draw the projectiles
+        projectile_manager.update(0, WIDTH, 0, HEIGHT, manager.enemies)
+        projectile_manager.draw()
 
         # Draw enemies and shooter
         manager.refresh_enemies()
