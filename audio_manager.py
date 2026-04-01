cff1523cc4f350bcc7f3ef194e757bea6960c2e7
Dillan <27839664@sun.ac.za>
2026-04-01 16:18:35 +0200

    Added audio functionality.

---
+++ audio_manager.py
@@ -0,0 +1,14 @@
+import stdaudio
+import math
+
+
+# 01/04/26: Dillan van Wyk: created the Audio_Manager class and play_tone function
+class Audio_Manager:
+    sample_rate = 44100
+
+    def play_tone(self, frequency, duration=0.2):
+        samples = [
+            math.sin(2 * math.pi * frequency * i / self.sample_rate)
+            for i in range(int(self.sample_rate * duration))
+        ]
+        stdaudio.playSamples(samples)
