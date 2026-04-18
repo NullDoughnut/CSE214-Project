026713a8f9029cfc2f64fc3e5c492a1529277506
Dillan <27839664@sun.ac.za>
2026-04-19 00:25:42 +0200

    Added constants file to store constants.

---
+++ constants.py
@@ -0,0 +1,8 @@
+# 19/04/26: Dillan van Wyk: Created constants file to store global constants
+
+WIDTH = 600
+HEIGHT = 600
+CANVAS_SIZE = 500
+TITLE_FONT = 35
+SUBTITLE_FONT = 25
+BODY_FONT = 18
---
+++ screen_manager.py
@@ -0,0 +1,130 @@
+import stddraw
+from picture import Picture as pic
+from constants import WIDTH, HEIGHT, TITLE_FONT, SUBTITLE_FONT, BODY_FONT
+
+menu_img = pic("assets/menu_img.png")
+
+
+# 18/04/26: Dillan van Wyk: Created Screen_Manager class to deal with all of the screens
+class Screen_Manager:
+    # 26/02/26: Dillan van Wyk: set up the title screen
+    # 09/04/26: Dillan van Wyk: changed background to an image
+    # 18/04/26: Dillan van Wyk: Added control instructions for player 2 and moved other text around
+    def draw_title_screen(self):
+        while True:
+            stddraw.clear()
+            stddraw.picture(menu_img, WIDTH / 2, HEIGHT / 2, WIDTH, HEIGHT)
+            stddraw.setPenColor(stddraw.WHITE)
+
+            stddraw.setFontSize(TITLE_FONT)
+            stddraw.text(WIDTH / 2, HEIGHT * 19 / 20, "SPACE INVADERS")
+
+            stddraw.setFontSize(SUBTITLE_FONT)
+            stddraw.text(WIDTH / 2, HEIGHT * 17 / 20, "Instructions:")
+
+            stddraw.setFontSize(BODY_FONT)
+            stddraw.text(
+                WIDTH / 2,
+                HEIGHT * 15 / 20,
+                "P1: [A] move left, [S] stop movement, [D] move right",
+            )
+            stddraw.text(
+                WIDTH / 2,
+                HEIGHT * 14 / 20,
+                "P1: [Q] rotate left, [W] stop rotation, [E] rotate right",
+            )
+            stddraw.text(WIDTH / 2, HEIGHT * 13 / 20, "P1: [Space] to shoot")
+
+            stddraw.text(
+                WIDTH / 2,
+                HEIGHT * 11 / 20,
+                "P2: [J] move left, [K] stop movement, [L] move right",
+            )
+            stddraw.text(
+                WIDTH / 2,
+                HEIGHT * 10 / 20,
+                "P2: [U] rotate left, [I] stop rotation, [O] rotate right",
+            )
+            stddraw.text(WIDTH / 2, HEIGHT * 9 / 20, "P2: [N] to shoot")
+
+            stddraw.text(WIDTH / 2, HEIGHT * 7 / 20, "[H] for help")
+            stddraw.text(WIDTH / 2, HEIGHT * 6 / 20, "[X] to quit")
+
+            stddraw.setFontSize(SUBTITLE_FONT)
+            stddraw.text(WIDTH / 2, HEIGHT * 4 / 20, "[1] Single Player   [2] Co-op")
+
+            stddraw.show(20)
+
+            # 18/04/26: Dillan van Wyk: Checks if the player(s) want to play single or multiplayer
+            if stddraw.hasNextKeyTyped():
+                key = stddraw.nextKeyTyped()
+                if key == "x":
+                    quit()
+                elif key == "1":
+                    return False
+                elif key == "2":
+                    return True
+                elif key == "h":
+                    self.draw_help()
+
+    # 18/04/26: Dillan van Wyk: Created help screen that displays the controls for both players 1 and 2. Simply copied title screen instructions
+    def draw_help(self):
+        while True:
+            stddraw.clear()
+            stddraw.picture(menu_img, WIDTH / 2, HEIGHT / 2, WIDTH, HEIGHT)
+            stddraw.setPenColor(stddraw.WHITE)
+
+            stddraw.setFontSize(TITLE_FONT)
+            stddraw.text(WIDTH / 2, HEIGHT * 18 / 20, "HELP")
+
+            stddraw.setFontSize(BODY_FONT)
+            stddraw.text(
+                WIDTH / 2,
+                HEIGHT * 15 / 20,
+                "P1: [A] move left, [S] stop movement, [D] move right",
+            )
+            stddraw.text(
+                WIDTH / 2,
+                HEIGHT * 14 / 20,
+                "P1: [Q] rotate left, [W] stop rotation, [E] rotate right",
+            )
+            stddraw.text(WIDTH / 2, HEIGHT * 13 / 20, "P1: [Space] to shoot")
+            stddraw.text(
+                WIDTH / 2,
+                HEIGHT * 11 / 20,
+                "P2: [J] move left, [K] stop movement, [L] move right",
+            )
+            stddraw.text(
+                WIDTH / 2,
+                HEIGHT * 10 / 20,
+                "P2: [U] rotate left, [I] stop rotation, [O] rotate right",
+            )
+            stddraw.text(WIDTH / 2, HEIGHT * 9 / 20, "P2: [N] to shoot")
+
+            stddraw.setFontSize(SUBTITLE_FONT)
+            stddraw.text(WIDTH / 2, HEIGHT * 5 / 20, "Press any key to resume")
+
+            stddraw.show(20)
+
+            if stddraw.hasNextKeyTyped():
+                key = stddraw.nextKeyTyped()
+                if key == "x":
+                    quit()
+                else:
+                    stddraw.nextKeyTyped()
+                    return None
+
+    # 27/03/26: Denlan Molokwu: Created the gameover and winner screen
+    def draw_game_over(self, width, height):
+        stddraw.clear()
+        stddraw.picture(menu_img, width / 2, height / 2, width, height)
+        stddraw.setPenColor(stddraw.WHITE)
+        stddraw.setFontSize(40)
+        stddraw.text(width / 2, height / 2, "GAME OVER")
+
+    def draw_winner(self, width, height):
+        stddraw.clear()
+        stddraw.picture(menu_img, width / 2, height / 2, width, height)
+        stddraw.setPenColor(stddraw.WHITE)
+        stddraw.setFontSize(40)
+        stddraw.text(width / 2, height / 2, "YOU ARE A WINNER")
